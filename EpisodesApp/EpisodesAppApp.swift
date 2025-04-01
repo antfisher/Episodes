5e5cc50beb65c507e9b09bd3cadceba800931121
Anton Rybaiev <anton.rybayev@gmail.com>
2025-04-01 17:11:50 -0600

    Initial Commit

---
+++ EpisodesApp/EpisodesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EpisodesAppApp.swift
+//  EpisodesApp
+//
+//  Created by Anton Rybaiev on 2025-04-01.
+//
+
+import SwiftUI
+
+@main
+struct EpisodesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
