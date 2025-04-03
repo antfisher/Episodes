05799128d3d7f7e076374036bf0f89f8dcdd0f19
Anton Rybaiev <anton.rybayev@gmail.com>
2025-04-02 22:56:52 -0600

    Add JSON data to simplify mock recreation if needed

--- EpisodesApp/Services/NetworkService/EpisodesNetworkService.swift
+++ EpisodesApp/Services/NetworkService/EpisodesNetworkService.swift
@@ -37,3 +37,28 @@ extension EpisodesNetworkService: EpisodesNetworkServiceType {
         try await ApiClient.shared.asyncRequest(endpoint: EpisodesEndpoints.episodeList)
     }
 }
+
+
+// Data used in this project, to easy recreate mock on Mocky (https://designer.mocky.io/) service if needed
+//[
+//  {
+//    "id": "1",
+//    "title": "Big Buck Bunny",
+//    "description": "This is the first sample video.",
+//    "videoUrl": "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+//    "duration": 596
+//  },
+//  {
+//    "id": "2",
+//    "title": "Elephants Dream",
+//    "videoUrl": "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+//    "duration": 653
+//  },
+//  {
+//    "id": "3",
+//    "title": "For Bigger Fun",
+//    "description": "This is the third sample video.",
+//    "videoUrl": "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+//    "duration": 60
+//  }
+//]
